23c5232b17f0c468f3dc163f01fa063d431d55af
DiMa <francev.dima@gmail.com>
2023-06-19 20:59:34 +0200

    перездача дз

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 "use strict";
 
 function fillArray() {
-    let rows = +prompt("Введіть кількість рядків: ");
-    let cols = +prompt("Введіть кількість стовпців: ");
-    if (isNaN(rows) || isNaN(cols)) {
-        alert("Кількість рядків та стовпців має бути числом");
+    const rows = +prompt("Введіть кількість рядків: ");
+    const cols = +prompt("Введіть кількість стовпців: ");
+    if (rows <= 0 || cols <= 0) {
+        alert("Кількість рядків та стовпців має бути цілим позитивним числом");
         return;
-    }
+    } 
 
-    let arr = [];
+    const arr = [];
     for (let i = 0; i < rows; i++) {
-        let rowArr = [];
+        const rowArr = [];
         for (let j = 0; j < cols; j++) {
-            let value = prompt(`Введіть значення для комірки [${i}][${j}]: `);
+            const value = prompt(`Введіть значення для комірки [${i}][${j}]: `);
             rowArr.push(value);
         }
         arr.push(rowArr);
     }
     return arr;
 }
-
-let myArray = fillArray();
+const myArray = fillArray();
 console.log(myArray);
